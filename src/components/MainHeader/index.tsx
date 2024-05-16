d318a3f2c77f3fa7f88590532fdae3a68a115ae3
Rafael Cunha <rafael.bispocunha@outlook.com>
2024-05-16 20:24:15 -0300

    Aula 12 - Criando o header principal

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,10 +1,22 @@
-import React from "react";
-import { Container } from './styles'
+import React, { useMemo } from "react";
+import emojis from "../../utils/emojis";
+
+import { Container, Profile, Welcome, UserName } from './styles'
 
 const MainHeader: React.FC = () => {
+   const emoji = useMemo(() =>{
+      const indice = Math.floor(Math.random() * emojis.length)
+      console.log(indice)
+      return emojis[indice]
+   }, []);
+
    return (
       <Container>
-         <h1>MainHeader</h1>
+         <h1>Toggle</h1>
+         <Profile>
+            <Welcome>Olá, {emoji}</Welcome>
+            <UserName>Rafael Cunha</UserName>
+         </Profile>
       </Container>
    )
 }

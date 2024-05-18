1efa135afa2d346d49ebc3c7cd91f0ac4c5eb1e2
Rafael Cunha <rafael.bispocunha@outlook.com>
2024-05-18 13:43:19 -0300

    Aula - 23 - Formatando valor monetário e data

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -7,6 +7,8 @@ import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
 import gains from "../../repositories/gains";
 import expenses from "../../repositories/expenses";
+import formatCurrency from "../../utils/formatCurrency";
+import formatDate from "../../utils/formatDate";
 
 import { Container, Content, Filters } from "./styles";
 
@@ -54,13 +56,14 @@ const List: React.FC = () => {
             return {
                 id: String(Math.random() * data.length),
                 description: item.description,
-                amountFormatted: item.amount,
+                amountFormatted: formatCurrency(Number(item.amount)),
                 frequency: item.frequency,
-                dateFormatted: item.date,
+                dateFormatted: formatDate(item.date),
                 tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
             }
-        })
-        setData(response)
+        });
+        setData(response);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
